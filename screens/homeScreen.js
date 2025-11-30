a50703cb785ac0e6f165d606780a7d741e946824
Fatma Selma Akpınar <akpinarselma1@gmail.com>
2025-11-30 23:19:41 +0300

    ilk commit

---
+++ screens/homeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Zamanlayici ekrani</Text>
+    </View>
+  );
+}
---
+++ screens/reportScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ReportScreen() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Rapor ekrani</Text>
+    </View>
+  );
+}
